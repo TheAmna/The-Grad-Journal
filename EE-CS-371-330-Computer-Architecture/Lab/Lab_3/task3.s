c7916a30a6b9109f8dd6399bb77f9fc1df1bb385
Amna Ali <amna.ali.23546@gmail.com>
2026-02-05 08:16:26 +0500

    task3.s

--- EE-CS-371-330-Computer-Architecture/Lab/Lab_3/task3.s
+++ EE-CS-371-330-Computer-Architecture/Lab/Lab_3/task3.s
@@ -1,9 +1,6 @@
-#task 3
-#x10 is array base address, x11 is index k
-#swaps v[k] and v[k+1]
 
-li x10, 0x100 #array base address
-li x11, 1   #k = 1 (so we swap v[1] and v[2])
+li x10, 0x100 #arr base address
+li x11, 0   #k = 1 (swap v[1] and v[2])
 li x14, 5 #first value
 li x15, 8 #second value
 
@@ -11,18 +8,15 @@ sw x14, 0(x10)  #v[0]=5
 sw x15, 4(x10)  #v[1]=8
 
 jal x1, SWAP
-j exit
+beq x0,x0,exit
 
 SWAP:
     slli x12, x11, 2  #k*4
     add x12, x10, x12 #address of v[k]
-    
-    lw x16, 0(x12) #temp = v[k]
-    lw x17, 4(x12) #load v[k+1]
-    
-    sw x17, 0(x12) # v[k] = v[k+1]
-    sw x16, 4(x12) # v[k+1] = temp
-    
-    jalr x0, 0(x1)     
+    lw x16, 0(x12)    #temp = v[k]
+    lw x17, 4(x12)    #load v[k+1]
+    sw x17, 0(x12)    #v[k] = v[k+1]
+    sw x16, 4(x12)    #v[k+1] = temp 
+    jalr x0, 0(x1)   
 
 exit:
